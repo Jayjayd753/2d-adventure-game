678653ecc1edb54f8f0a410510012a64abe56c41
Jayjayd753 <JAydend170@nycstudents.net>
2025-01-15 09:13:29 -0500

    part 18 play animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
-
-     
-
-   // Variables related to player character movement
-   public InputAction MoveAction;
-   Rigidbody2D rigidbody2d;
-   Vector2 move;
-   public float speed = 3.0f;
+  // Variables related to player character movement
+  public InputAction MoveAction;
+  Rigidbody2D rigidbody2d;
+  Vector2 move;
+  public float speed = 3.0f;
 
 
   // Variables related to the health system
@@ -22,9 +18,15 @@ public class PlayerController : MonoBehaviour
   public int health { get { return currentHealth; }}
 
 
- //Variables for Player Animation
- Animator animator;
- //Vector2
+  // Variables related to temporary invincibility
+  public float timeInvincible = 2.0f;
+  bool isInvincible;
+  float damageCooldown;
+
+
+  // Variables related to animation
+  Animator animator;
+  Vector2 moveDirection = new Vector2(1,0);
 
 
   // Start is called before the first frame update
@@ -32,22 +34,39 @@ public class PlayerController : MonoBehaviour
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
-    animator = GetComponent<Animator>();
-    currentHealth = 1;
-   }
+     animator = GetComponent<Animator>();
 
 
+     currentHealth = maxHealth;
+  }
  
   // Update is called once per frame
   void Update()
-   {
-      move = MoveAction.ReadValue<Vector2>();
+  {
+     move = MoveAction.ReadValue<Vector2>();
+
+
+      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        {
+           moveDirection.Set(move.x, move.y);
+           moveDirection.Normalize();
+        }
+
+
+     animator.SetFloat("Look X", moveDirection.x);
+     animator.SetFloat("Look Y", moveDirection.y);
+     animator.SetFloat("Speed", move.magnitude);
+
+
+     if (isInvincible)
+       {
+           damageCooldown -= Time.deltaTime;
+           if (damageCooldown < 0)
+               isInvincible = false;
+       }
    }
 
-
-
-
-  // FixedUpdate has the same call rate as the physics system
+// FixedUpdate has the same call rate as the physics system
   void FixedUpdate()
   {
      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -57,12 +76,19 @@ public class PlayerController : MonoBehaviour
 
   public void ChangeHealth (int amount)
   {
+     if (amount < 0)
+       {
+           if (isInvincible)
+               return;
+          
+           isInvincible = true;
+           damageCooldown = timeInvincible;
+           animator.SetTrigger("Hit");
+       }
+
+
      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
      UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
-     Debug.Log(currentHealth + "/" + maxHealth);
   }
 
-
-
-     
 }
